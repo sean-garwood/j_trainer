94f2cffc631d864597877619552d9c75b0918bcc
sean-garwood <seansgarwood@gmail.com>
2024-06-14 18:14:57 -0400

    feat: user controller
    * update README

--- README.md
+++ README.md
@@ -1,24 +1,10 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+## features
 
-Things you may want to cover:
+Display all jeopardy clues
 
-* Ruby version
+### todo
 
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+* Fix pagination of clues
+  * hotfix: clues?page=n
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
   get '/clues', to: 'clues#index'
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get 'up' => 'rails/health#show', as: :rails_health_check
-
-  # Defines the root path route ("/")
   resources :clues, only: [:index]
   resources :users, only: %i[new create]
   root to: 'clues#index'
