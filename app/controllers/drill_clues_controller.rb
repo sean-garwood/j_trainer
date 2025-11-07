dfea49af2b6bcd4175ec272acd7608f51847ad04
sean-garwood <seansgarwood@gmail.com>
2025-11-06 21:22:48 -0500

    fix: drill controllers

---
+++ app/controllers/drill_clues_controller.rb
@@ -0,0 +1,43 @@
+class DrillCluesController < ApplicationController
+  before_action :set_drill
+
+  def create
+    @drill_clue = @drill.drill_clues.build(drill_clue_params)
+
+    if @drill_clue.save
+      # Fetch next clue
+      @clue = @drill.fetch_clue
+
+      if @clue.nil?
+        # No more clues - end drill
+        @drill.update(ended_at: Time.current)
+        session[:current_drill_id] = nil
+        redirect_to drill_path(@drill), notice: "Drill completed!"
+      else
+        # Prepare next drill clue (unsaved)
+        @drill_clue = DrillClue.new(drill: @drill, clue: @clue)
+
+        # Render Turbo Frame with next clue
+        respond_to do |format|
+          format.turbo_stream
+          format.html { redirect_to train_drills_path }
+        end
+      end
+    else
+      # Validation failed
+      @clue = @drill_clue.clue
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_drill
+    @drill = current_user.drills.find(params[:drill_id])
+    authorize! :update, @drill
+  end
+
+  def drill_clue_params
+    params.expect(drill_clue: [ :clue_id, :response, :response_time ])
+  end
+end
