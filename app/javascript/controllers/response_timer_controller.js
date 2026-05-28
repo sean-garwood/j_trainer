d5ab6c584b8c149298ae1191cbeb4cfbf913b294
Sean Garwood <seansgarwood@gmail.com>
2026-05-28 12:25:43 -0400

    chore(deps): bump tailwind to v4.3
    - add htmlbeautifier for erb fmt
    - js indent 2 => 4 spaces

--- app/javascript/controllers/response_timer_controller.js
+++ app/javascript/controllers/response_timer_controller.js
@@ -1,64 +1,64 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["timeField", "input", "countdown"];
-  // TODO: unmagic defaults
-  static values = {
-    maxResponseTime: { type: Number, default: 15 },
-    maxBuzzTime: { type: Number, default: 5 },
-  };
+    static targets = ["timeField", "input", "countdown"];
+    // TODO: unmagic defaults
+    static values = {
+        maxResponseTime: { type: Number, default: 15 },
+        maxBuzzTime: { type: Number, default: 5 },
+    };
 
-  get now() {
-    return new Date().getTime();
-  }
-
-  connect() {
-    this.clueDisplayTime = this.now;
-    this.responseTime = null;
-    this.maxBuzzTime = this.maxBuzzTimeValue;
-    this.maxResponseTime = this.maxResponseTimeValue;
-    this.startResponseCountdown();
-  }
-
-  startResponseCountdown() {
-    this.timeLeft = this.maxResponseTime;
-    this.updateCountdown();
-
-    const countdown = setInterval(() => {
-      this.timeLeft -= 1;
-      this.updateCountdown();
-
-      if (this.timeLeft <= 0) {
-        clearInterval(this.responseInterval);
-        this.countdownTarget.textContent = "Time's up!";
-        this.inputTarget.disabled = true;
-        this.timeFieldTarget.value = this.maxResponseTime;
-        // FIXIT: does not seem to submit the request properly
-        this.element.requestSubmit();
-      }
-    }, 1000);
-    this.responseInterval = countdown;
-  }
-
-  updateCountdown() {
-    const secondsLeft = this.timeLeft.toFixed();
-    this.countdownTarget.textContent = `Time remaining: ${secondsLeft}s`;
-  }
-
-  beforeSubmit(event) {
-    const endTime = this.now;
-    const responseTime = (endTime - this.clueDisplayTime) / 1000;
-    this.timeFieldTarget.value = responseTime;
-    this.clearIntervals();
-  }
-
-  disconnect() {
-    this.clearIntervals();
-  }
-
-  clearIntervals() {
-    if (this.responseInterval) {
-      clearInterval(this.responseInterval);
+    get now() {
+        return new Date().getTime();
     }
-  }
+
+    connect() {
+        this.clueDisplayTime = this.now;
+        this.responseTime = null;
+        this.maxBuzzTime = this.maxBuzzTimeValue;
+        this.maxResponseTime = this.maxResponseTimeValue;
+        this.startResponseCountdown();
+    }
+
+    startResponseCountdown() {
+        this.timeLeft = this.maxResponseTime;
+        this.updateCountdown();
+
+        const countdown = setInterval(() => {
+            this.timeLeft -= 1;
+            this.updateCountdown();
+
+            if (this.timeLeft <= 0) {
+                clearInterval(this.responseInterval);
+                this.countdownTarget.textContent = "Time's up!";
+                this.inputTarget.disabled = true;
+                this.timeFieldTarget.value = this.maxResponseTime;
+                // FIXIT: does not seem to submit the request properly
+                this.element.requestSubmit();
+            }
+        }, 1000);
+        this.responseInterval = countdown;
+    }
+
+    updateCountdown() {
+        const secondsLeft = this.timeLeft.toFixed();
+        this.countdownTarget.textContent = `Time remaining: ${secondsLeft}s`;
+    }
+
+    beforeSubmit(event) {
+        const endTime = this.now;
+        const responseTime = (endTime - this.clueDisplayTime) / 1000;
+        this.timeFieldTarget.value = responseTime;
+        this.clearIntervals();
+    }
+
+    disconnect() {
+        this.clearIntervals();
+    }
+
+    clearIntervals() {
+        if (this.responseInterval) {
+            clearInterval(this.responseInterval);
+        }
+    }
 }
